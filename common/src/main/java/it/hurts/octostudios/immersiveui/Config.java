b75bc4ee11cdcb6f5953f3a31911626c80ab4bc1
catboybinary <catboybinary@disroot.org>
2024-08-23 16:21:41 +0300

    1.20.1 config update

--- common/src/main/java/it/hurts/octostudios/immersiveui/Config.java
+++ common/src/main/java/it/hurts/octostudios/immersiveui/Config.java
@@ -1,40 +1,42 @@
 package it.hurts.octostudios.immersiveui;
 
 
+import it.hurts.octostudios.octolib.modules.config.annotations.Prop;
+import it.hurts.octostudios.octolib.modules.config.impl.OctoConfig;
 import lombok.Data;
 
 @Data
-public class Config { //implements OctoConfig {
-    //@Prop(comment = "Enables the hotbar selector animation.")
+public class Config implements OctoConfig {
+    @Prop(comment = "Enables the hotbar selector animation.")
     private boolean enableHotbarSelectorAnimation = true;
 
-    //@Prop(comment = "Enables floating item rotation.")
+    @Prop(comment = "Enables floating item rotation.")
     private boolean enableFloatingItemRotation = true;
 
-    //@Prop(comment = "Enables hovering of matching items.")
+    @Prop(comment = "Enables hovering of matching items.")
     private boolean enableMatchingItemHovering = true;
     
-    //@Prop(comment = "Enables particles for rare items.")
+    @Prop(comment = "Enables particles for rare items.")
     private boolean enableRarityParticles = true;
 
-    //@Prop(comment = "Enables vanilla slot highlighting")
+    @Prop(comment = "Enables vanilla slot highlighting")
     private boolean enableVanillaSlotHighlighting = true;
 
-    //@Prop(comment = "Affects the speed of the hotbar selector.")
+    @Prop(comment = "Affects the speed of the hotbar selector.")
     private double hotbarSelectorSpeed = 3d;
 
-    //@Prop(comment = "Affects the size of the hovered item.")
+    @Prop(comment = "Affects the size of the hovered item.")
     private float hoveredItemScale = 1.4f;
 
-    //@Prop(comment = "Affects the hover amplitude of items, that match to the item that is carried in the cursor.")
+    @Prop(comment = "Affects the hover amplitude of items, that match to the item that is carried in the cursor.")
     private float matchingItemHoverAmplitude = 0.8f;
 
-    //@Prop(comment = "Affects the easing speed, that is applied to the rotation of the floating item.")
+    @Prop(comment = "Affects the easing speed, that is applied to the rotation of the floating item.")
     private float floatingItemEasingSpeed = 0.75f;
 
-    //@Prop(comment = "Affects the rotation amplitude of the floating item.")
+    @Prop(comment = "Affects the rotation amplitude of the floating item.")
     private float floatingItemRotationAmplitude = 1f;
 
-    //@Prop(comment = "Moves the hotbar selector above the items. You might want to disable this, if you use resource packs that change the default selector texture.")
+    @Prop(comment = "Moves the hotbar selector above the items. You might want to disable this, if you use resource packs that change the default selector texture.")
     private boolean renderHotbarSelectorAboveItems = true;
 }
--- forge/src/main/java/it/hurts/octostudios/immersiveui/forge/ImmersiveUIForge.java
+++ forge/src/main/java/it/hurts/octostudios/immersiveui/forge/ImmersiveUIForge.java
@@ -1,10 +1,13 @@
 package it.hurts.octostudios.immersiveui.forge;
 
 import it.hurts.octostudios.immersiveui.ImmersiveUI;
+import it.hurts.octostudios.octolib.modules.config.ConfigManager;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod(ImmersiveUI.MOD_ID)
-//@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public final class ImmersiveUIForge {
     public ImmersiveUIForge() {
 //        // Submit our event bus to let Architectury API register our content on the right time.
@@ -13,9 +16,9 @@ public final class ImmersiveUIForge {
         // Run our common setup.
         ImmersiveUI.init();
     }
-//
-//    @SubscribeEvent
-//    public static void setup(FMLCommonSetupEvent event) {
-//        ConfigManager.registerConfig("immersiveui", ImmersiveUI.CONFIG);
-//    }
+
+    @SubscribeEvent
+    public static void setup(FMLCommonSetupEvent event) {
+        ConfigManager.registerConfig("immersiveui", ImmersiveUI.CONFIG);
+    }
 }
